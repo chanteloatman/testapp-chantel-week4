c89460b9b39e96efdbbf0394faca83cca5f36e85
Oatman <c_oatman@tss-hr420-dm30.fc.ca>
2018-02-12 12:36:59 -0500

    commit changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+const templateTypes = [
+  { name: "Jade", usefulness : "sucky" },
+  { name: "EJS", usefulness : "a little better" },
+  { name: "Handlebars", usefulness : "awesome" }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', message : "Jade templates are used in views folder to show text" });
+  res.render('index', {
+    title: 'Express',
+    message : "JI like EJS", //Jade templates are used in views folder to show text
+    templates : templateTypes
+  });
 });
 
 module.exports = router;
